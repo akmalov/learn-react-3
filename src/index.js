2b97a220fdc7042b6ec8f15362ab681e3ac85753
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-03 20:28:18 +0300

    learn functional components

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ var ReactDOM = require('react-dom');
 //var App = React.createElement('h1', null, 'Hello World');
 var App = <h1>Hello World</h1>;
 
-ReactDOM.render(App, document.getElementById('root'));
+var Component = function() {
+  return (
+    <h1>Hello World</h1>
+  );
+};
+
+ReactDOM.render(
+  <Component />,
+  document.getElementById('root')
+);
