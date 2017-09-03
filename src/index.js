5560171e208e6da7f2d044d36f7d03a58bbb37e9
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-03 20:50:24 +0300

    learn props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 //var App = React.createElement('h1', null, 'Hello World');
-var App = <h1>Hello World</h1>;
+//var App = <h1>Hello World</h1>;
 
-var Component = function() {
+var Component = function (props) {
   return (
-    <h1>Hello World</h1>
+    <div style={{ color: props.color }}>
+      <h1>{props.greeting}</h1>
+    </div>
   );
 };
 
 ReactDOM.render(
-  <Component />,
+  <Component greeting="Hello world" color="green" />,
   document.getElementById('root')
 );
