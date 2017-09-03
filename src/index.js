482689c6b79a22d35a09b92018fc054593b27385
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-03 19:54:33 +0300

    learn jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var App = React.createElement('h1', null, 'Hello World');
+//var App = React.createElement('h1', null, 'Hello World');
+var App = <h1>Hello World</h1>;
 
 ReactDOM.render(App, document.getElementById('root'));
