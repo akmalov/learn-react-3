57bf7207c3caaf152d27e644c1ae3727104d4945
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-03 21:05:29 +0300

    learn class components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createClass = require('create-react-class');
 
-//var App = React.createElement('h1', null, 'Hello World');
-//var App = <h1>Hello World</h1>;
-
-var Component = function (props) {
-  return (
-    <div style={{ color: props.color }}>
-      <h1>{props.greeting}</h1>
-    </div>
-  );
-};
+// var Component = function (props) {
+//   return (
+//     <div style={{ color: props.color }}>
+//       <h1>{props.greeting}</h1>
+//     </div>
+//   );
+// };
+var Component = createClass({
+  render: function() {
+    return (
+      <div style={{color: props.color}}>
+        <h1>{this.props.greeting}</h1>
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
   <Component greeting="Hello world" color="green" />,
