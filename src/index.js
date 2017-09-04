7d4840177c2f4d7be42400dec993195d24a948d5
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-04 16:14:30 +0300

    add shows app with props and state

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var createClass = require('create-react-class');
-
-// var Component = function (props) {
-//   return (
-//     <div style={{ color: props.color }}>
-//       <h1>{props.greeting}</h1>
-//     </div>
-//   );
-// };
-var Component = createClass({
-  getInitialState: function () {
-    return {
-      color: 'blue'
-    };
-  },
-  handleButtonClick: function () {
-    this.setState(function (prevState) {
-      return {
-        color: (prevState.color === 'blue') ? 'green' : 'blue'
-      };
-    });
-  },
-  render: function () {
-    return (
-      <div>
-        <div style={{color: this.state.color}}>
-          <h1>{this.props.greeting}</h1>
-        </div>
-        <button onClick={this.handleButtonClick}>ClickMe</button>
-      </div>
-    );
-  }
-});
+var Show = require('./Show');
+var shows = require('./shows');
 
 ReactDOM.render(
-  <Component greeting="Hello world"/>,
+  <Show show={shows} />,
   document.getElementById('root')
 );
